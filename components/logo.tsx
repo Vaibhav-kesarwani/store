52400f48903646c72131cafb74abedd070da5c95
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-22 22:23:53 +0530

    chore: make the logo for the site

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,22 @@
-const Logo = () => {
-  return <div>Logo</div>;
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const Logo = ({ className }: { className?: string }) => {
+  return (
+    <Link href={"/"}>
+      <h2
+        className={cn(
+          "text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",
+          className
+        )}
+      >
+        Dev{" "}
+        <span className="text-shop_light_green group-hover:text-shop_dark_green hoverEffect">
+          Bazaar
+        </span>
+      </h2>
+    </Link>
+  );
 };
 
 export default Logo;
