45854270ab45ee1bf920d5014440dfddbdd16891
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-23 17:46:07 +0530

    chore: make the mobile navigation side bar

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,27 @@
+import CartIcon from "./cart-icon";
 import Container from "./container";
+import FavoriteIcon from "./favorite-icon";
 import HeaderMenu from "./header-menu";
+import MobileMenu from "./mobile-menu";
 import Logo from "./logo";
+import SearchBar from "./search-bar";
+import SignIn from "./sign-in";
 
 const Header = () => {
   return (
-    <header className="bg-white py-5">
-      <Container className="flex items-center justify-between">
-        <Logo />
+    <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
+      <Container className="flex items-center justify-between text-lightColor">
+        <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+          <MobileMenu />
+          <Logo />
+        </div>
         <HeaderMenu />
-        <div>Others</div>
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+          <FavoriteIcon />
+          <SignIn />
+        </div>
       </Container>
     </header>
   );
