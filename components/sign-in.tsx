c42f1781d27ab8ac548596126b15d979fef95dd3
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-23 17:44:55 +0530

    chore: make the right menu bar for the site

---
+++ components/sign-in.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+  return (
+    <button className="text-sm text-shop_dark_green font-semibold hover:text-shop_light_green hoverEffect">
+      Sign In
+    </button>
+  );
+}
